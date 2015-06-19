3d5d4dcaebf8ff3fec8385bfae9f433eeb3a5b60
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-06-20 00:12:09 +0300

    Make ugly, but working version of frontend-info.

--- src/bin/frontend-info.rs
+++ src/bin/frontend-info.rs
@@ -1,4 +1,9 @@
 
 extern crate linuxdvb;
 
-fn main() {}
+use linuxdvb::{Frontend,ReadWriteMode,BlockMode};
+
+fn main() {
+    let frontend = Frontend::open("/dev/dvb/adapter0/frontend0", ReadWriteMode::ReadOnly, BlockMode::NonBlocking).unwrap();
+    println!("{:?}", frontend.get_info().unwrap());
+}
