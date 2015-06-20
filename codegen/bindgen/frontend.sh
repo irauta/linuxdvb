f439cbea44251bc8766e1f021292f896050fe32d
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-06-20 19:12:31 +0300

    Add licensing information.

--- codegen/bindgen/frontend.sh
+++ codegen/bindgen/frontend.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright 2015 Ilkka Rauta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script outputs a C header to stdout, intended to be used with rust-bindgen to generate
 # basic data structures and constants. This is very much an ugly hack, specific only to the file
 # linux/dvb/frontend.h and may become outdated as the actual header is updated in futre. To atone
