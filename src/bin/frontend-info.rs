9f033793d5dd0d39b2abd1ad9bb7dd000b446eee
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-06-24 23:04:37 +0300

    Improve the frontend-info program.

--- src/bin/frontend-info.rs
+++ src/bin/frontend-info.rs
@@ -14,9 +14,65 @@
 
 extern crate linuxdvb;
 
-use linuxdvb::{Frontend,ReadWriteMode,BlockMode};
+use std::fs;
+use std::error::Error;
+use std::path::Path;
+
+use linuxdvb::{Frontend,ReadWriteMode,BlockMode,properties};
+use linuxdvb::properties::GetProperty as GP;
+use linuxdvb::properties::GetPropertyValue as GPV;
+
+type SimpleResult = Result<(), Box<Error>>;
+
+fn show_frontend_info(path: &Path) -> SimpleResult {
+    let frontend = Frontend::open("/dev/dvb/adapter0/frontend0", ReadWriteMode::ReadOnly, BlockMode::NonBlocking).unwrap();
+    println!("Device: {}", path.to_string_lossy());
+    let info = try!(frontend.get_info());
+    println!("Name: {}", info.name);
+
+    let properties = try!(frontend.get_properties(&[
+        GP::DtvEnumDelsys
+        ]));
+    if let GPV::DtvEnumDelsys(delsys) = properties[0] {
+        println!("Supported delivery systems: {:?}", delsys.delivery_systems());
+    }
+
+    println!("Minimal frequency: {}", info.frequency_min);
+    println!("Maximal frequency: {}", info.frequency_max);
+    println!("Frequency step: {}", info.frequency_stepsize);
+    println!("Frequency tolerance: {}", info.frequency_tolerance);
+    println!("Minimal symbol rate: {}", info.symbol_rate_min);
+    println!("Maximal symbol rate: {}", info.symbol_rate_max);
+    println!("Maximal symbol rate tolerance: {}", info.symbol_rate_tolerance);
+
+    Ok(())
+}
+
+fn iterate_devices(adapter_dir: &Path) -> SimpleResult {
+    for device in try!(fs::read_dir(adapter_dir)) {
+        let device = try!(device);
+        let is_frontend = device.path().file_name()
+            .map(|f| f.to_str())
+            .unwrap_or(None)
+            .map_or(false, |f| f.starts_with("frontend"));
+        if is_frontend {
+            try!(show_frontend_info(&device.path()));
+        }
+    }
+    Ok(())
+}
+
+fn iterate_adapters() -> SimpleResult {
+    for adapter_dir in try!(fs::read_dir("/dev/dvb")) {
+        let adapter_dir = try!(adapter_dir);
+        try!(iterate_devices(&adapter_dir.path()));
+    }
+    Ok(())
+}
 
 fn main() {
-    let frontend = Frontend::open("/dev/dvb/adapter0/frontend0", ReadWriteMode::ReadOnly, BlockMode::NonBlocking).unwrap();
-    println!("{:?}", frontend.get_info().unwrap());
+    match iterate_adapters() {
+        Ok(_) => (),
+        Err(error) => println!("Error: {}", error.description())
+    }
 }
