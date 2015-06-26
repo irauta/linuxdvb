aa2907a00bb94d323b8573ce85b6bedb3b3b4bb9
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-06-27 00:58:54 +0300

    Refactor and expand the frontend caps type(s).

--- src/frontend/caps.rs
+++ src/frontend/caps.rs
@@ -16,38 +16,66 @@
 
 use super::ffi as ffi;
 
-bitflags!(
-      flags FrontendCaps: u32 {
-            const IsStupid = ffi::FE_IS_STUPID,
-            const CanInversionAuto = ffi::FE_CAN_INVERSION_AUTO,
-            const CanFec12 = ffi::FE_CAN_FEC_1_2,
-            const CanFec23 = ffi::FE_CAN_FEC_2_3,
-            const CanFec34 = ffi::FE_CAN_FEC_3_4,
-            const CanFec45 = ffi::FE_CAN_FEC_4_5,
-            const CanFec56 = ffi::FE_CAN_FEC_5_6,
-            const CanFec67 = ffi::FE_CAN_FEC_6_7,
-            const CanFec78 = ffi::FE_CAN_FEC_7_8,
-            const CanFec89 = ffi::FE_CAN_FEC_8_9,
-            const CanFecAuto = ffi::FE_CAN_FEC_AUTO,
-            const CanQpsk = ffi::FE_CAN_QPSK,
-            const CanQam16 = ffi::FE_CAN_QAM_16,
-            const CanQam32 = ffi::FE_CAN_QAM_32,
-            const CanQam64 = ffi::FE_CAN_QAM_64,
-            const CanQam128 = ffi::FE_CAN_QAM_128,
-            const CanQam256 = ffi::FE_CAN_QAM_256,
-            const CanQamAuto = ffi::FE_CAN_QAM_AUTO,
-            const CanTransmissionModeAuto = ffi::FE_CAN_TRANSMISSION_MODE_AUTO,
-            const CanBandwidthAuto = ffi::FE_CAN_BANDWIDTH_AUTO,
-            const CanGuardIntervalAuto = ffi::FE_CAN_GUARD_INTERVAL_AUTO,
-            const CanHierarchyAuto = ffi::FE_CAN_HIERARCHY_AUTO,
-            const Can8vsb = ffi::FE_CAN_8VSB,
-            const Can16vsb = ffi::FE_CAN_16VSB,
-            const HasExtendedCaps = ffi::FE_HAS_EXTENDED_CAPS,
-            const CanMultistream = ffi::FE_CAN_MULTISTREAM,
-            const CanTurboFec = ffi::FE_CAN_TURBO_FEC,
-            const Can2gModulation = ffi::FE_CAN_2G_MODULATION,
-            const NeedsBending = ffi::FE_NEEDS_BENDING,
-            const CanRecover = ffi::FE_CAN_RECOVER,
-            const CanMuteTs = ffi::FE_CAN_MUTE_TS,
-      }
+macro_rules! frontend_caps (
+    ({
+        $($variant:ident => $value:path),+
+    }) => (
+        bitflags!(
+            flags FrontendCaps: u32 {
+                $(const $variant = $value),+
+            }
+        );
+        #[derive(Debug,Copy,Clone)]
+        pub enum FrontendCapsEnum {
+            $($variant),+
+        }
+        impl Into<Vec<FrontendCapsEnum>> for FrontendCaps {
+            fn into(self) -> Vec<FrontendCapsEnum> {
+                let mut caps = Vec::new();
+                if self.is_empty() {
+                    return caps;
+                }
+                $(
+                    if !$variant.is_empty() && self.contains($variant) {
+                        caps.push(FrontendCapsEnum::$variant);
+                    }
+                )+
+                caps
+            }
+        }
+    )
 );
+
+frontend_caps!({
+    IsStupid => ffi::FE_IS_STUPID,
+    CanInversionAuto => ffi::FE_CAN_INVERSION_AUTO,
+    CanFec12 => ffi::FE_CAN_FEC_1_2,
+    CanFec23 => ffi::FE_CAN_FEC_2_3,
+    CanFec34 => ffi::FE_CAN_FEC_3_4,
+    CanFec45 => ffi::FE_CAN_FEC_4_5,
+    CanFec56 => ffi::FE_CAN_FEC_5_6,
+    CanFec67 => ffi::FE_CAN_FEC_6_7,
+    CanFec78 => ffi::FE_CAN_FEC_7_8,
+    CanFec89 => ffi::FE_CAN_FEC_8_9,
+    CanFecAuto => ffi::FE_CAN_FEC_AUTO,
+    CanQpsk => ffi::FE_CAN_QPSK,
+    CanQam16 => ffi::FE_CAN_QAM_16,
+    CanQam32 => ffi::FE_CAN_QAM_32,
+    CanQam64 => ffi::FE_CAN_QAM_64,
+    CanQam128 => ffi::FE_CAN_QAM_128,
+    CanQam256 => ffi::FE_CAN_QAM_256,
+    CanQamAuto => ffi::FE_CAN_QAM_AUTO,
+    CanTransmissionModeAuto => ffi::FE_CAN_TRANSMISSION_MODE_AUTO,
+    CanBandwidthAuto => ffi::FE_CAN_BANDWIDTH_AUTO,
+    CanGuardIntervalAuto => ffi::FE_CAN_GUARD_INTERVAL_AUTO,
+    CanHierarchyAuto => ffi::FE_CAN_HIERARCHY_AUTO,
+    Can8vsb => ffi::FE_CAN_8VSB,
+    Can16vsb => ffi::FE_CAN_16VSB,
+    HasExtendedCaps => ffi::FE_HAS_EXTENDED_CAPS,
+    CanMultistream => ffi::FE_CAN_MULTISTREAM,
+    CanTurboFec => ffi::FE_CAN_TURBO_FEC,
+    Can2gModulation => ffi::FE_CAN_2G_MODULATION,
+    NeedsBending => ffi::FE_NEEDS_BENDING,
+    CanRecover => ffi::FE_CAN_RECOVER,
+    CanMuteTs => ffi::FE_CAN_MUTE_TS
+});
